543e2f1a0581181252896a99a2764fe654050e69
Andrey Almashi <andrey.almashi@gmail.com>
2023-04-12 16:14:25 +0300

    A conditional authorization template has been created. The structure of the repository has been changed

--- LegalCompany/app/controllers/statements_controller.rb
+++ LegalCompany/app/controllers/statements_controller.rb
@@ -1,5 +1,10 @@
 class StatementsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
     @statements = Statement.all
+  end
+
+  def show
+    @statement = Statement.find(params[:id])
   end
 end
--- LegalCompany/config/routes.rb
+++ LegalCompany/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_scope :user do
+    get '/signout', to: 'devise/sessions#destroy', as: :signout
+  end
   # Defines the root path route ("/")
   # root "articles#index"
   root "statements#index"
