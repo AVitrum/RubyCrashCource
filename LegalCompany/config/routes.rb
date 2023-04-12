2ec2e024c40a9ff58216cad34b8bc3503d16c84a
Andrey Almashi <andrey.almashi@gmail.com>
2023-04-12 22:05:21 +0300

    A request database has been created and it is possible to add them. Added ability to edit requests and added minimal design

--- LegalCompany/config/routes.rb
+++ LegalCompany/config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
+  resources :requests
+  get 'home/info'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    get '/signout', to: 'devise/sessions#destroy', as: :signout
-  end
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "statements#index"
-  resources :statements
+  root "home#index"
 end
